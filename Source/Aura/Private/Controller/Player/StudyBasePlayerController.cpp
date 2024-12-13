898b2cdadda77144fc062968b665d2f0322b8580
Solemmbum <81722124+Solemmbum@users.noreply.github.com>
2024-12-13 15:23:19 -0300

    Created Movement Input and Controller.
    -Created the Base Player Controller class.
    -Created the Input Action for Movement.
    -Created the default Input Mapping Context.

---
+++ Source/Aura/Private/Controller/Player/StudyBasePlayerController.cpp
@@ -0,0 +1,4 @@
+// // Copyright (c) 2024 @ Solemmbum Games - @All Rights Reserved.
+
+
+#include "Controller/Player/StudyBasePlayerController.h"
---
+++ Source/Aura/Public/Controller/Player/StudyBasePlayerController.h
@@ -0,0 +1,17 @@
+// // Copyright (c) 2024 @ Solemmbum Games - @All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "StudyBasePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AStudyBasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
