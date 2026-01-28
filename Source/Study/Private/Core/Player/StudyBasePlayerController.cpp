73f8fbba4a1740beee85bd0e8e2558fdfa735a10
Solemmbum <81722124+Solemmbum@users.noreply.github.com>
2026-01-28 19:10:53 -0300

    Setup the Movement Input on the Player Controller

--- Source/Study/Private/Core/Player/StudyBasePlayerController.cpp
+++ Source/Study/Private/Core/Player/StudyBasePlayerController.cpp
@@ -4,6 +4,7 @@
 #include "Core/Player/StudyBasePlayerController.h"
 
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AStudyBasePlayerController::AStudyBasePlayerController()
 {
@@ -28,4 +29,31 @@ void AStudyBasePlayerController::BeginPlay()
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+void AStudyBasePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AStudyBasePlayerController::Move);
+}
+
+void AStudyBasePlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	APawn* ControlledPawn = GetPawn();
+	if (!IsValid(ControlledPawn))
+	{
+		return;
+	}
+	
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation(0.f, GetControlRotation().Yaw, 0.f);
+	
+	const FVector ForwardDirection = FRotationMatrix(Rotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(Rotation).GetUnitAxis(EAxis::Y);
+	
+	ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+	ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 }
--- Source/Study/Public/Core/Player/StudyBasePlayerController.h
+++ Source/Study/Public/Core/Player/StudyBasePlayerController.h
@@ -6,7 +6,10 @@
 #include "GameFramework/PlayerController.h"
 #include "StudyBasePlayerController.generated.h"
 
+struct FInputActionValue;
+
 class UInputMappingContext;
+class UInputAction;
 
 /**
  * 
@@ -20,8 +23,14 @@ private:
 	UPROPERTY(EditAnywhere, Category = "Input")
 	TObjectPtr<UInputMappingContext> BaseInputContext;
 	
+	UPROPERTY(EditAnywhere, Category = "Input|Movement")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	void Move(const FInputActionValue& InputActionValue);
+	
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 	
 public:
 	AStudyBasePlayerController();
