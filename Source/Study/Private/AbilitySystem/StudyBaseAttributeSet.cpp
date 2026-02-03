726974eb78ec4bca051d81c900eb22e0a351c05b
Solemmbum <81722124+Solemmbum@users.noreply.github.com>
2026-02-03 17:34:32 -0300

    Added a few Attributes to the Attribute Set

--- Source/Study/Private/AbilitySystem/StudyBaseAttributeSet.cpp
+++ Source/Study/Private/AbilitySystem/StudyBaseAttributeSet.cpp
@@ -3,3 +3,40 @@
 
 #include "AbilitySystem/StudyBaseAttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UStudyBaseAttributeSet::UStudyBaseAttributeSet()
+{
+	
+}
+
+void UStudyBaseAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME_CONDITION_NOTIFY(UStudyBaseAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UStudyBaseAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UStudyBaseAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UStudyBaseAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UStudyBaseAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UStudyBaseAttributeSet, Health, OldHealth);
+}
+
+void UStudyBaseAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UStudyBaseAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UStudyBaseAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UStudyBaseAttributeSet, Mana, OldMana);
+}
+
+void UStudyBaseAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UStudyBaseAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/Study/Public/AbilitySystem/StudyBaseAttributeSet.h
+++ Source/Study/Public/AbilitySystem/StudyBaseAttributeSet.h
@@ -14,4 +14,39 @@ class STUDY_API UStudyBaseAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+private:
+	UPROPERTY(ReplicatedUsing = OnRep_Health, Getter)
+	FGameplayAttributeData Health;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_MaxHealth, Getter)
+	FGameplayAttributeData MaxHealth;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_Mana, Getter)
+	FGameplayAttributeData Mana;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_MaxMana, Getter)
+	FGameplayAttributeData MaxMana;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+	
+public:
+	UStudyBaseAttributeSet();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	FGameplayAttributeData GetHealth() const { return Health; }
+	FGameplayAttributeData GetMaxHealth() const { return MaxHealth; }
+	FGameplayAttributeData GetMana() const { return Mana; }
+	FGameplayAttributeData GetMaxMana() const { return MaxMana; }
+	
 };
