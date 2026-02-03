286aca46d8e83bb3bf29b3dbbc34d13ca5d85df6
Solemmbum <81722124+Solemmbum@users.noreply.github.com>
2026-02-03 16:03:26 -0300

    Created the Base Player State Class for the Project

---
+++ Source/Study/Private/Core/Player/StudyBasePlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Solemmbum
+
+
+#include "Core/Player/StudyBasePlayerState.h"
+
+AStudyBasePlayerState::AStudyBasePlayerState()
+{
+	SetNetUpdateFrequency(100.f);
+}
---
+++ Source/Study/Public/Core/Player/StudyBasePlayerState.h
@@ -0,0 +1,20 @@
+// Copyright Solemmbum
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "StudyBasePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUDY_API AStudyBasePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+public:
+	AStudyBasePlayerState();
+	
+};
